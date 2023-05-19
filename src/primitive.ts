b5aac68aa98746c96523caa43b915605c9f6c5fc
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-05-19 12:52:05 +0600

    practice : primitive type in typescrpt

---
+++ src/primitive.ts
@@ -0,0 +1,79 @@
+
+// string type : 
+let countryName: string; 
+countryName = "Bangladesh"; 
+countryName = "India"; 
+countryName = "Nepal"; 
+//  shows errors for : 
+/* 
+countryName = 20; // because the type is string; 
+countryName = false // because the type is boolean ;
+countryName = undefined // because the type is undefined; 
+
+ */
+
+
+// Number Type : 
+let myAge: number; 
+myAge = 20; 
+myAge = 21; 
+
+// Show errors 
+
+/* 
+   myAge = '20'; // because "20"'s type is string 
+   myAge = 'tweenty ' // because the type is string ; 
+   myAge = false // because the type is boolean; 
+   myAge = undefined // because the type is undefined; 
+
+*/
+
+
+//   Boolean Type: 
+let isHappy: boolean; 
+isHappy = true; 
+isHappy = false; 
+isHappy = myAge === 20; 
+isHappy = !myAge && !countryName; 
+isHappy = !!myAge && !!countryName; 
+isHappy = !!myAge || !!countryName; 
+
+
+// Shows errors for : 
+// isHappy = 10; // because the type is number 
+// isHappy = 'yes'// because the type  gives errors 
+
+
+
+//   undefined Type :
+
+let xyz:undefined;
+
+xyz = undefined; 
+const b = undefined; 
+xyz = b; 
+
+// Shows errors for : 
+/* 
+   xyz = 20; 
+   xyz = 'undefined'; 
+   xyz = null 
+   xyz = false 
+   xyz =true ; 
+ */
+
+
+
+//  Null Type : 
+
+let Empty:null; 
+
+Empty = null; 
+/* 
+   Empty = "null"; 
+   Empty = 20; 
+   Empty = "anything" 
+   Empty = false 
+   Empty =true ; 
+   Empty = undefined
+ */
