0b3192e5147dd3ee6c0a93ee2878b10a999e2e95
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-05-29 22:30:44 +0600

    feat: union.ts includes for practice union type data

---
+++ src/union.ts
@@ -0,0 +1,24 @@
+let myAge1: number | string;
+myAge1 = 1;
+myAge1 = "one";
+myAge1 = "ratul hossain";
+myAge1 = 2;
+
+// myAge1 = false; // give errors
+// myAge1 = true // give errors
+// myAge1 = []; // gives errors
+// myAge1 = {} gives errors;
+// myAge1 = undefined; // gives error; 
+// myAge1 = null // gives error; 
+
+
+let MyFriend : "amjad" | "faisal"; 
+
+MyFriend = "amjad"; 
+MyFriend = "faisal"; 
+
+// Gives error for other values : 
+// MyFriend = "mostafizur rahaman"; // gives errors 
+// MyFriend = 'ratul hossain'; 
+// MyFriend = 29; 
+
